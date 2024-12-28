bea85da945c7c4c3ae6261b84a2ce47f98662d31
Akash Yadav <itsaky01@gmail.com>
2024-12-28 16:29:16 +0530

    fix: do not build llvm and wasi when building locally

--- scripts/build.sh
+++ scripts/build.sh
@@ -44,35 +44,37 @@ fi
 source "$HOME/.cargo/env"
 cargo install --force --vers 0.26.0 cbindgen
 
-# Build LLVM
-pushd $llvm
-llvmtarget=$(cat "$llvm/targets_to_build")
-echo "building llvm for $llvmtarget"
-if grep -q "Fedora" /etc/os-release; then
-    cmake -S llvm -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=out -DCMAKE_C_COMPILER=clang-18 \
-        -DCMAKE_CXX_COMPILER=clang++-18 -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_TARGETS_TO_BUILD="$llvmtarget" \
-        -DLLVM_USE_LINKER=lld -DLLVM_BINUTILS_INCDIR=/usr/include -DLLVM_ENABLE_PLUGINS=FORCE_ON \
-        -DLLVM_DEFAULT_TARGET_TRIPLE="x86_64-unknown-linux-gnu"
-else
-    cmake -S llvm -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=out -DCMAKE_C_COMPILER=clang-16 \
-        -DCMAKE_CXX_COMPILER=clang++-16 -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_TARGETS_TO_BUILD="$llvmtarget" \
-        -DLLVM_USE_LINKER=lld -DLLVM_BINUTILS_INCDIR=/usr/include -DLLVM_ENABLE_PLUGINS=FORCE_ON \
-        -DLLVM_DEFAULT_TARGET_TRIPLE="x86_64-unknown-linux-gnu"
-fi
-cmake --build build -j"$(nproc)"
-cmake --build build --target install -j"$(nproc)"
-popd
+if [[ "$fdroid_build" == "true" ]]; then
+    # Build LLVM
+    pushd $llvm
+    llvmtarget=$(cat "$llvm/targets_to_build")
+    echo "building llvm for $llvmtarget"
+    if grep -q "Fedora" /etc/os-release; then
+        cmake -S llvm -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=out -DCMAKE_C_COMPILER=clang-18 \
+            -DCMAKE_CXX_COMPILER=clang++-18 -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_TARGETS_TO_BUILD="$llvmtarget" \
+            -DLLVM_USE_LINKER=lld -DLLVM_BINUTILS_INCDIR=/usr/include -DLLVM_ENABLE_PLUGINS=FORCE_ON \
+            -DLLVM_DEFAULT_TARGET_TRIPLE="x86_64-unknown-linux-gnu"
+    else
+        cmake -S llvm -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=out -DCMAKE_C_COMPILER=clang-16 \
+            -DCMAKE_CXX_COMPILER=clang++-16 -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_TARGETS_TO_BUILD="$llvmtarget" \
+            -DLLVM_USE_LINKER=lld -DLLVM_BINUTILS_INCDIR=/usr/include -DLLVM_ENABLE_PLUGINS=FORCE_ON \
+            -DLLVM_DEFAULT_TARGET_TRIPLE="x86_64-unknown-linux-gnu"
+    fi
+    cmake --build build -j"$(nproc)"
+    cmake --build build --target install -j"$(nproc)"
+    popd
 
-# Build WASI SDK
-pushd "$wasi"
-mkdir -p build/install/wasi
-touch build/compiler-rt.BUILT # fool the build system
-make \
-    PREFIX=/wasi \
-    build/wasi-libc.BUILT \
-    build/libcxx.BUILT \
-    -j"$(nproc)"
-popd
+    # Build WASI SDK
+    pushd "$wasi"
+    mkdir -p build/install/wasi
+    touch build/compiler-rt.BUILT # fool the build system
+    make \
+        PREFIX=/wasi \
+        build/wasi-libc.BUILT \
+        build/libcxx.BUILT \
+        -j"$(nproc)"
+    popd
+fi
 
 # Build microG libraries
 pushd "$gmscore"
